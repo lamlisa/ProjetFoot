6d3d4808a6c766d37ea7d9086e6c460d85f3ed65
Lisa Lam <llisa.sdb@gmail.com>
2018-02-15 15:39:10 +0100

    jeudi 15/02

--- module/Comportement.py
+++ module/Comportement.py
@@ -16,4 +16,14 @@ class Comportement(object):
 
 	def passe(self,id_player,id_team):
 		t = Tools(self.state)
-		return SoccerAction(t.vect_ball_player(id_player,id_team),(t.le_plus_proche(id_player,id_team) - self.state.player_state(id_team,id_player).position).normalize() * 4.)
+		return SoccerAction((t.le_plus_proche_ami(id_player,id_team) - self.state.player_state(id_team,id_player).position).normalize() * 0.2,(t.le_plus_proche_ami(id_player,id_team) - self.state.player_state(id_team,id_player).position).normalize() * 2.)
+
+	def se_demarquer(self,id_player,id_team):
+		t=Tools(self.state)
+		return SoccerAction((self.state.player_state(id_team,id_player).position-t.le_plus_proche_ennemi(id_player,id_team)).normalize() * 0.2,Vector2D(0,0))
+
+	def marquer_joueur(self, id_player, id_team):
+		t=Tools(self.state)
+		return SoccerAction((t.le_plus_proche_ennemi(id_player,id_team)-self.state.player_state(id_team,id_player).position).normalize() * 0.2,Vector2D(0,0))
+
+
--- module/Tools.py
+++ module/Tools.py
@@ -28,10 +28,12 @@ class Tools(object):
 		else:
 			return self.state.ball.position.x > (GAME_WIDTH/2.)
 
-	def le_plus_proche(self,id_player,id_team):
+	def le_plus_proche_ami(self,id_player,id_team):
 		L = [(it, ip) for (it, ip) in self.state.players if ((it == id_team) & (ip != id_player))]
 		s=0
 		v=0
+		if len(L)==1:
+			return self.state.player_state(id_team,L[0][1]).position
 		for i in range(len(L)-1):
 			if i==0:
 				s= self.state.player_state(id_team,id_player).position.distance(self.state.player_state(id_team,L[0][1]).position)
@@ -41,5 +43,30 @@ class Tools(object):
 				s=u
 				v=L[i+1][1]
 		return self.state.player_state(id_team,v).position
+
+	def le_plus_proche_ennemi(self,id_player,id_team):
+		L = [(it, ip) for (it, ip) in self.state.players if it != id_team]
+		s=0
+		v=0
+		if len(L)==1:
+			return self.state.player_state(id_team,L[0][1]).position
+		for i in range(len(L)-1):
+			if i==0:
+				s= self.state.player_state(id_team,id_player).position.distance(self.state.player_state(id_team,L[0][1]).position)
+				v=L[0][1]
+			u=self.state.player_state(id_team,id_player).position.distance(self.state.player_state(id_team,L[i+1][1]).position)
+			if s>u:
+				s=u
+				v=L[i+1][1]
+		return self.state.player_state(id_team,v).position
+
+
+	def est_dans_perimetre(self,id_player,id_team):
+		L=[(it, ip) for (it, ip) in self.state.players if it != id_team]
+		u = False
+		for i in range(len(L)):
+			if (self.state.player_state(L[i][0],L[i][1]).position).x > (self.state.player_state(id_team,id_player).position).x-5. and (self.state.player_state(L[i][0],L[i][1]).position).x < (self.state.player_state(id_team,id_player).position).x+5. and (self.state.player_state(L[i][0],L[i][1]).position).y > (self.state.player_state(id_team,id_player).position).y-5. and (self.state.player_state(L[i][0],L[i][1]).position).y < (self.state.player_state(id_team,id_player).position).y+5.:
+				u = True
+		return u
 
 
