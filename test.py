86f83e816168cfec6e1941b387743cac31e80510
Lisa Lam <llisa.sdb@gmail.com>
2018-02-19 19:33:41 +0100

    lundi 19/02

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ from module.Strategy import *
 ## Creation d'une equipe
 pyteam = SoccerTeam(name="PyTeam")
 thon = SoccerTeam(name="ThonTeam")
+pyteam.add("PyPlayer",FonceurStrategy())
 pyteam.add("PyPlayer",DefenseurStrategy())
 thon.add("ThonPlayer",FonceurStrategy())
-
+thon.add("ThonPlayer22222",DefenseurStrategy())
 
 #Creation d'une partie
 simu = Simulation(pyteam,thon)
