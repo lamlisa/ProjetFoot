74ec82c2a2bbc55a0d05ad2cf3311d8d18b82057
Lisa Lam <llisa.sdb@gmail.com>
2018-03-01 13:25:54 +0100

    jeudi 01/03

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from soccersimulator import SoccerTeam, Simulation, Player
 from soccersimulator import show_simu
-from module.Strategy import *
+from module.strategy import *
 
 ## Creation d'une equipe
 pyteam = SoccerTeam(name="PyTeam")
 thon = SoccerTeam(name="ThonTeam")
-pyteam.add("PyPlayer",FonceurStrategy())
-pyteam.add("PyPlayer",DefenseurStrategy())
-thon.add("ThonPlayer",FonceurStrategy())
-thon.add("ThonPlayer22222",DefenseurStrategy())
+
+#pyteam.add("PyDefenseur",DefenseurStrategy())
+#pyteam.add("PyFonceur1",Fonceur1Strategy())
+#pyteam.add("PyFonceur2",Fonceur2Strategy())
+pyteam.add("PyFonceur3",Fonceur3Strategy())
+
+#thon.add("ThonFonceur1",Fonceur1Strategy())
+#thon.add("ThonFonceur2",Fonceur2Strategy())
+#thon.add("ThonDefenseur",DefenseurStrategy())
+thon.add("ThonFonceur3",Fonceur3Strategy())
 
 #Creation d'une partie
 simu = Simulation(pyteam,thon)
